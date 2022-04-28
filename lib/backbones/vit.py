f604da6bb2d5ba897226a6362ab27e74673e6300
fukexue <fukexuefdu@163.com>
2022-04-28 16:27:20 +0800

    vit

--- lib/backbones/vit.py
+++ lib/backbones/vit.py
@@ -351,7 +351,7 @@ def vit_base_patch16(pretrained=False, **kwargs):
     model.default_cfg = default_cfgs['vit_base_patch16_224']
     if pretrained:
         load_pretrained(
-            model, num_classes=model.num_classes, in_chans=kwargs.get('in_chans', 3), filter_fn=_conv_filter)
+            model, num_classes=model.num_classes, in_chans=kwargs.get('in_chans', 3), filter_fn=_conv_filter, strict=False)
     return model
 
 
