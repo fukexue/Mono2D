3b397880dc7e9ad1cb6171d556860ba8631b4cec
fukexue <fukexuefdu@163.com>
2022-04-28 16:15:02 +0800

    vit

--- lib/backbones/vit.py
+++ lib/backbones/vit.py
@@ -344,10 +344,14 @@ def _conv_filter(state_dict, patch_size=16):
 
 
 
-def vit_base_patch16(**kwargs):
+def vit_base_patch16(pretrained=False, **kwargs):
     model = VisionTransformer(
         patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
         norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    model.default_cfg = default_cfgs['vit_base_patch16_224']
+    if pretrained:
+        load_pretrained(
+            model, num_classes=model.num_classes, in_chans=kwargs.get('in_chans', 3), filter_fn=_conv_filter)
     return model
 
 
